5330e21f792a1bb697e4d88880d9af09111650bf
Abuzar-Asif <Abuzar@pixarsart.com>
2025-01-09 17:42:40 +0500

    react router dom using use location hook

--- src/ReactRouter/About.jsx
+++ src/ReactRouter/About.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const About = () => {
+    const loca=useLocation()
+
   return (
     <div>
       <h1>
         This is about page
       </h1>
+      <p>
+      THIS IS ID :  {loca.state.id}
+      </p>
     </div>
   )
 }
--- src/ReactRouter/Contact.jsx
+++ src/ReactRouter/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Contact = () => {
+    const id=5;
     const navigate =useNavigate()
 
   const toAbout=()=>{
-    navigate('/About')
+    navigate('/About',{state:{id:id}})
+   
   }
   return (
     <div>
@@ -15,7 +17,7 @@ const Contact = () => {
       <button onClick={toAbout}>navigate to about </button>
   
       <h1>
-        Using use Navigate below 
+        Using use Navigate below  and sending data with use location 
      </h1>
         
             <button onClick={toAbout}>About</button>
